d61e6dcf02502c92e3fbca302cf21846605224f9
Pushkar8171 <pushkarsinghrathore81@gmail.com>
2023-09-20 15:15:08 +0530

    Running migration so as to get the schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_091127) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_094143) do
+  create_table "authors", force: :cascade do |t|
+    t.string "f_name"
+    t.string "l_name"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "year_published"
+    t.string "isbn"
+    t.decimal "price"
+    t.boolean "out_of_print"
+    t.integer "views"
+    t.integer "supplier_id"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "lastname"
@@ -19,6 +40,32 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_091127) do
     t.integer "visits"
     t.integer "orders_count"
     t.integer "lock_version"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.time "date_submitted"
+    t.integer "status"
+    t.decimal "subtotal"
+    t.decimal "shipping"
+    t.decimal "tax"
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "rating"
+    t.integer "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
