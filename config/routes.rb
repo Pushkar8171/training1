4708d506756858702fb155295463df4e0c90ac17
Pushkar8171 <pushkarsinghrathore81@gmail.com>
2023-09-26 12:58:04 +0530

    Adding the requires controller:ORDERS and establishing desired routes for them

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/customer/:id", to: "customer#index"
+  get "/customer/:id", to: "customer#index" 
+  get "orders/:id", to: "orders#index"
 end
