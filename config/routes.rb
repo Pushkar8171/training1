77875e9b660b2e874757dee89f1d5473558b6e58
Pushkar8171 <pushkarsinghrathore81@gmail.com>
2023-10-02 14:45:49 +0530

    Added searc form on Index page / Understanding the Forms in more detail

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "books#index"
   #get "/customer/:id", to: "customer#index" 
   #get "orders/:id", to: "orders#index" as: "orders"
   #resources :orders, :customer
